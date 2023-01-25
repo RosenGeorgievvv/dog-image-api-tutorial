7318ec7cf4131850026164b6dea6f9dd0ed1d090
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-01-25 10:34:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const dogImageDiv = document.getElementById('dogImage')
 const dogButton = document.getElementById('dogButton')
 
 
-
 const getNewDog = () => {
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
